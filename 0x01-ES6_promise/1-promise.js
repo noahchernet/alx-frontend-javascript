2cdf993b71d4137bfd6b8aaf8b167aef7473bfde
Noah Chernet <noahchernet@yahoo.com>
2022-06-15 12:23:40 +0300

    fix: eslint error in 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
-const getFullResponseFromAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else reject(Error('The fake API is not working currently'));
-  });
-};
+const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => {
+  if (success) {
+    resolve({
+      status: 200,
+      body: 'Success',
+    });
+  } else reject(Error('The fake API is not working currently'));
+});
 
 export default getFullResponseFromAPI;
