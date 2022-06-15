5732ad1b12b223dbf3868d5f31097299f6bb9b04
Noah Chernet <noahchernet@yahoo.com>
2022-06-15 14:48:40 +0300

    Done task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from '../util';
+
+export default function handleProfileSignup() {
+  let returnedStr = '';
+  uploadPhoto()
+    .then(({ body }) => {
+      returnedStr += `${body} `;
+      createUser().then(({ firstName, lastName }) => {
+        returnedStr += `${firstName} ${lastName}`;
+      });
+    })
+    .catch(() => console.log('Signup system offline'))
+    .finally(() => {
+      console.log(returnedStr);
+    });
+}
