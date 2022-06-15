0adf71e5a69696a3affc227f2d93c0e5e3a3432f
Noah Chernet <noahchernet@yahoo.com>
2022-06-15 17:32:22 +0300

    1-block-scoped.js: error surpressed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-line no-unused-vars
     const task = true;
-    // eslint-disable-line no-unused-vars
     const task2 = false;
   }
 
