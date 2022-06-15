f9311935c30a81648e12f6e0dfa7b77aa5080a40
Noah Chernet <noahchernet@yahoo.com>
2022-06-15 15:04:04 +0300

    fix: eslint add comma to 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(
-        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`,
       );
     })
     .catch(() => {
