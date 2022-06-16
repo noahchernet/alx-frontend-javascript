9f761cf7ea326ed4c530536a17061d9ec9cd8a9a
Noah Chernet <noahchernet@yahoo.com>
2022-06-16 16:41:51 +0300

    Trying task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {}
+}
