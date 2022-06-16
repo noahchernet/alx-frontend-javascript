53da649dc569fb6bc0aa1df091e7bb9caa6bfc8a
Noah Chernet <noahchernet@yahoo.com>
2022-06-16 21:43:20 +0300

    2-hbtn_course: typechecking in setter methods, and type checking students

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('Name must be a string');
-    if (typeof length !== 'number') throw TypeError('Length must be a number');
-    // if (typeof students !== 'array') throw TypeError('');
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an object');
+    }
 
     this._name = name;
     this._length = length;
@@ -23,14 +27,21 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     this._name = name;
   }
 
   set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
   set students(students) {
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an object');
+    }
     this._students = students;
   }
 }
