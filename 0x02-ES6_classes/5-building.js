b6420554c00c3d06f861f63d5ca8ed55eadde503
Noah Chernet <noahchernet@yahoo.com>
2022-06-16 21:55:19 +0300

    Trying task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,12 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.getAllFuncs(this).indexOf('evacuationWarningMessage') === -1)
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
   }
 
   get sqft() {
@@ -10,6 +15,20 @@ export default class Building {
 
   set sqft(value) {
     this._sqft = value;
+  }
+
+  getAllFuncs(toCheck) {
+    const props = [];
+    let obj = this;
+    do {
+      props.push(...Object.getOwnPropertyNames(obj));
+      // eslint-disable-next-line no-cond-assign
+    } while ((obj = Object.getPrototypeOf(obj)));
+
+    // eslint-disable-next-line array-callback-return
+    return props.sort().filter((e, i, arr) => {
+      if (e !== arr[i + 1] && typeof toCheck[e] === 'function') return true;
+    });
   }
 
   // eslint-disable-next-line class-methods-use-this
