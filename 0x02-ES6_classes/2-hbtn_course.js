8993f0cda07119b403087c9505b24245f98e4b24
Noah Chernet <noahchernet@yahoo.com>
2022-06-16 15:15:04 +0300

    Trying task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    // if (typeof students !== 'array') throw TypeError('');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
