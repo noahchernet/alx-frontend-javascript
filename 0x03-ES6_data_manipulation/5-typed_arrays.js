e14ec00756109594bdab198811ec4b18de0cf9e8
Noah Chernet <noahchernet@yahoo.com>
2022-06-20 21:39:56 +0300

    Done task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) return new Error('Position outside range');
+
+  const buffer = new ArrayBuffer(length);
+  const int8view = new Int8Array(buffer);
+  int8view[position] = value;
+
+  return buffer;
+}
