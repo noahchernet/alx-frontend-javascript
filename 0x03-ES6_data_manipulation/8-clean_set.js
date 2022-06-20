22ee5645f43b06b9e5abe186fa9549fe84310ba7
Noah Chernet <noahchernet@yahoo.com>
2022-06-20 22:19:27 +0300

    task 8: added null check and type check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,9 @@
 // that start with a specific string (startString).
 export default function cleanSet(set, startString) {
   return startString !== ''
+    && startString !== undefined
+    && startString !== null
+    && typeof startString !== 'string'
     ? [...set]
       .filter((item) => item.slice(0, startString.length) === startString)
       .map((item) => item.slice(startString.length))
