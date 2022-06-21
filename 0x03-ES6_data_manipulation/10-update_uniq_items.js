db163ad74d2c3aab2897c5e700b52e7a213b35ab
Noah Chernet <noahchernet@yahoo.com>
2022-06-21 16:51:34 +0300

    Done task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(map) {
+  if (map.constructor.name !== 'Map') throw new Error('Cannot process');
+
+  for (const key of map.keys()) {
+    if (map.get(key) === 1) {
+      map.set(key, 100);
+    }
+  }
+}
