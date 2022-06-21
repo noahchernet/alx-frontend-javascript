c7c163ece885d20994560141600d4cccbb0bdab7
Noah Chernet <noahchernet@yahoo.com>
2022-06-21 16:19:51 +0300

    task 8: retrying

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
   return startString !== ''
     && startString !== undefined
     && startString !== null
-    && typeof startString !== 'string'
+    && typeof startString === 'string'
     ? [...set]
       .filter((item) => item.slice(0, startString.length) === startString)
       .map((item) => item.slice(startString.length))
