1333c04f4cd4f7197eea906e1ea628e65a950977
Noah Chernet <noahchernet@yahoo.com>
2022-06-21 17:05:28 +0300

    Done advanced task

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,7 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.get(endpoint)) weakMap.set(endpoint, 1);
+  else if (weakMap.get(endpoint) === 5) throw new Error('Endpoint load is high');
+  else weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+}
