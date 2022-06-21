0c15ed5ae74db642619757607be97135644b879d
Noah Chernet <noahchernet@yahoo.com>
2022-06-21 17:12:52 +0300

    100-weak.js: greater than 5 api requests also checked

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,6 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (!weakMap.get(endpoint)) weakMap.set(endpoint, 1);
-  else if (weakMap.get(endpoint) === 5) throw new Error('Endpoint load is high');
+  else if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
   else weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
